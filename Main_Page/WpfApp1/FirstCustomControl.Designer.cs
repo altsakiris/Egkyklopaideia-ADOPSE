03caf2a1d4eae8d4eee8b56f153a44b43082bf63
Thanasis <it154474@it.teithe.gr>
2019-01-07 22:59:18 +0200

    Populated the homepage with random articles from the database.

--- Main_Page/WpfApp1/FirstCustomControl.Designer.cs
+++ Main_Page/WpfApp1/FirstCustomControl.Designer.cs
@@ -28,33 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 117);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 174);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(332, 189);
+            this.label1.Location = new System.Drawing.Point(15, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Κεντρική Σελίδα;";
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Read more...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(280, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(253, 174);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Read more...";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label22";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(539, 117);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(253, 174);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(161, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Read more...";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
             // 
             // FirstCustomControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FirstCustomControl";
             this.Size = new System.Drawing.Size(817, 423);
             this.Load += new System.EventHandler(this.FirstCustomControl_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Main_Page/WpfApp1/FirstCustomControl.cs
+++ Main_Page/WpfApp1/FirstCustomControl.cs
@@ -12,15 +12,46 @@ namespace Egkyklopaideia
 {
     public partial class FirstCustomControl : UserControl
     {
+        //int count = source.Split('/').Length - 1;
+        SqlConn conn = new SqlConn();
+        List<GroupBox> groupBoxes = new List<GroupBox>();
+        List<Label> LabeList = new List<Label>();
+        List<Button> ButtonList = new List<Button>();
         public FirstCustomControl()
         {
             InitializeComponent();
+            groupBoxes.Add(groupBox1);
+            groupBoxes.Add(groupBox2);
+            groupBoxes.Add(groupBox3);
+            LabeList.Add(label1);
+            LabeList.Add(label2);
+            LabeList.Add(label3);
+            ButtonList.Add(button1);
+            ButtonList.Add(button2);
+            ButtonList.Add(button3);
+            Populate_Main_form();
         }
-
-        private void label2_Click(object sender, EventArgs e)
+        
+        private void Populate_Main_form()
         {
-
+            int i = 0,c=0,y=0;
+            foreach (var groupbox in groupBoxes)
+            {
+                groupbox.Text = "This is box number " + (i+1);
+                i++;
+            }
+            foreach (var label in LabeList)
+            {
+                label.Text = "Label " + (c + 1);
+                c++;
+            }
+            foreach (var button in ButtonList)
+            {
+                button.Text = "btn" + (y + 1);
+                y++;
+            }
         }
+       
 
         private void FirstCustomControl_Load(object sender, EventArgs e)
         {
